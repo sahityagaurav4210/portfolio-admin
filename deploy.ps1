cd111747b9c17955a4d4b004f04326cad359bd66
Gaurav Sahitya <gauravsahitya.rockymountaintech@gmail.com>
2025-01-31 22:54:30 +0530

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -20,7 +20,7 @@ if (Test-Path -Path $credentialPath) {
   }
   
   Write-Output "Preparing the image..."
-  docker build -t $($credentials.ImgName) .
+  docker build --build-args VITE_API_TIMEOUT=$($credentials.VITE_API_TIMEOUT) --build-args VITE_API_BASE_URL=$($credentials.VITE_API_BASE_URL) --build-args VITE_APP_ENV=$($credentials.VITE_APP_ENV) -t $($credentials.ImgName) .
   docker push $($credentials.ImgName)
 
   Write-Output "Deploying the app, please wait..."
@@ -57,8 +57,11 @@ else {
   $hashedPwd = Read-Host "Enter your caprover password" -AsSecureString
   $appName = Read-Host "Enter your app name" 
   $imgName = Read-Host "Enter your docker image name (without docker username)"
+  $viteApiBaseUrl = Read-Host "Enter your api base url";
+  $viteAppEnv = Read-Host "Enter your app environment"
+  $viteApiTimeout = Read-Host "Enter your api timeout"
 
-  docker build -t "$dockerUsername/$imgName" .
+  docker build --build-args VITE_API_BASE_URL=$viteApiBaseUrl --build-args VITE_APP_ENV=$viteAppEnv --build-args VITE_API_TIMEOUT=$viteApiTimeout -t "$dockerUsername/$imgName" .
   docker push "$dockerUsername/$imgName"
   
   Write-Output "Deploying the app, please wait..."
@@ -69,10 +72,13 @@ else {
   caprover deploy -h "$uri" -p "$plainPwd" -i "$dockerUsername/$imgName" --appName "$appName"
 
   $fileContents = @{
-    Host     = $uri
-    Password = $plainPwd
-    AppName  = $appName
-    ImgName  = "$dockerUsername/$imgName"
+    Host              = $uri
+    Password          = $plainPwd
+    AppName           = $appName
+    ImgName           = "$dockerUsername/$imgName"
+    VITE_APP_ENV      = $viteAppEnv
+    VITE_API_BASE_URL = $viteApiBaseUrl
+    VITE_API_TIMEOUT  = $viteApiTimeout
   }
   $data = $fileContents | ConvertTo-Json -Depth 10
 
