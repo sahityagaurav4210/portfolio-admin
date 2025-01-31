10a6aa86eb13ecb907f2f3e8eb33a55242dfa387
Gaurav Sahitya <gauravsahitya.rockymountaintech@gmail.com>
2025-01-31 23:01:24 +0530

    temp commit

--- deploy.ps1
+++ deploy.ps1
@@ -20,7 +20,7 @@ if (Test-Path -Path $credentialPath) {
   }
   
   Write-Output "Preparing the image..."
-  docker build --build-args VITE_API_TIMEOUT=$($credentials.VITE_API_TIMEOUT) --build-args VITE_API_BASE_URL=$($credentials.VITE_API_BASE_URL) --build-args VITE_APP_ENV=$($credentials.VITE_APP_ENV) -t $($credentials.ImgName) .
+  docker build -t $($credentials.ImgName) .
   docker push $($credentials.ImgName)
 
   Write-Output "Deploying the app, please wait..."
@@ -57,11 +57,8 @@ else {
   $hashedPwd = Read-Host "Enter your caprover password" -AsSecureString
   $appName = Read-Host "Enter your app name" 
   $imgName = Read-Host "Enter your docker image name (without docker username)"
-  $viteApiBaseUrl = Read-Host "Enter your api base url";
-  $viteAppEnv = Read-Host "Enter your app environment"
-  $viteApiTimeout = Read-Host "Enter your api timeout"
 
-  docker build --build-args VITE_API_BASE_URL=$viteApiBaseUrl --build-args VITE_APP_ENV=$viteAppEnv --build-args VITE_API_TIMEOUT=$viteApiTimeout -t "$dockerUsername/$imgName" .
+  docker build -t "$dockerUsername/$imgName" .
   docker push "$dockerUsername/$imgName"
   
   Write-Output "Deploying the app, please wait..."
